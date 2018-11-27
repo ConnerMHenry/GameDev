4b66690ff4f4cc7579ba8a98f82ab8f4ca597a4a
Michael Maxner <mmaxner@rogers.com>
2018-11-27 11:50:29 -0500

    Menu and stuff

---
+++ GameDevA3/Assets/Scripts/UI/BackButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackButton : MonoBehaviour {
+	public GameObject BackTo;
+	public GameObject BackFrom;
+	// Use this for initialization
+	public void Go()
+	{
+		BackTo.SetActive(true);
+		BackFrom.SetActive(false);
+	}
+}
