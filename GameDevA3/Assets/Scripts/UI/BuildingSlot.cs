1779b36612707537cc59f04be7f83f2fcd49d01d
Conner Henry <connerhenry.91@gmail.com>
2018-11-26 19:12:50 -0500

    Update to BuildingWindow, 50% functional

---
+++ GameDevA3/Assets/Scripts/UI/BuildingSlot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingSlot : MonoBehaviour {
+
+    public string Name;
+    public Image spriteImage;
+    public Building building;
+    public Color disabledColor;
+    public BuildingWindowController controller;
+
+    private bool isAvailable = false;
+    public bool IsAvailable {
+        get {
+            return isAvailable;
+        }
+        set {
+            ToggleAvailability(value);
+            isAvailable = true;
+        }
+    }
+
+    private void ToggleAvailability(bool available) {
+        spriteImage.color = available ? Color.white : disabledColor;
+    }
+
+    public void OnPointerOver() {
+        if (controller != null)
+        {
+            controller.DisplayBuilding(this);
+        }
+    }
+}
--- GameDevA3/Assets/Scripts/UI/BuildingWindowController.cs
+++ GameDevA3/Assets/Scripts/UI/BuildingWindowController.cs
@@ -1,19 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class BuildingWindowController : MonoBehaviour {
+    public Button buildButton;
+    public TextMeshProUGUI BuildingName;
+    public TextMeshProUGUI BuildingDesc;
 
-    List<Building> buildingOptions;
+    public BuildingSlot defaultSlot;
 
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    void Start () {
+        if (defaultSlot != null) {
+            DisplayBuilding(defaultSlot);
+        }
+        buildButton.interactable = false;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
+    public void DisplayBuilding(BuildingSlot buildingSlot) {
+        BuildingName.text = buildingSlot.Name;
+        BuildingDesc.text = buildingSlot.building.Description;
+        buildButton.interactable = buildingSlot.IsAvailable;
+    }
 }
